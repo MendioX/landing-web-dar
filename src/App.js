db3fe76d766167b2a90ad659db1826ff48df454e
MendioX <82616448+MendioX@users.noreply.github.com>
2024-06-10 20:15:04 -0300

    retomando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
-import './App.css';
+
+import Navbar from "./Components/Navbar";
+import Nosotros from './Layauts/nosotros';
+
+import Footer from './Components/Footer';
+import Partners from './Layauts/Partners';
+
+import Inicio from './Layauts/Inicio';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Campanas from './Components/Campanas';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <BrowserRouter>
+        
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <Navbar />
+                <Inicio />
+                <Nosotros />
+                <Partners />
+                <Footer />
+              </div>
+            }
+          />
+          <Route path="/campains" element={<div>
+            <Navbar />
+            <Campanas />
+            <Footer />
+          </div>} />
+          <Route path="/newCampain" element={<Campanas />} />
+          <Route path="/campains" element={<Campanas />} />
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
